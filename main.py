f364c6d1e5b18d80e080bed08167b2a0c2d29c2c
Todd Birchard <toddbirchard@gmail.com>
2020-08-14 22:40:06 -0400

    Updated main script entry point.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 """Application entry point."""
 from shibabot import create_bot
-from config import DISCORD_TOKEN
 
 
 bot = create_bot()
 
 if __name__ == '__main__':
-    bot.run(DISCORD_TOKEN)
+    bot
--- shibabot/__init__.py
+++ shibabot/__init__.py
@@ -3,6 +3,7 @@ from .log import LOGGER
 from discord.ext import commands as discord_cmd
 from .commands import bot_commands
 from .events import bot_events
+from config import DISCORD_TOKEN
 
 
 def create_bot():
@@ -10,5 +11,6 @@ def create_bot():
     bot = discord_cmd.Bot(command_prefix='!')
     bot = bot_events(bot)
     bot = bot_commands(bot)
+    bot.run(DISCORD_TOKEN)
 
     return bot
